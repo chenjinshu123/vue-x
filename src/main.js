b31a89a9d2fc9db130700a4576f2c18fb6a11930
chenjinshu <957071770@qq.com>
2020-01-01 20:55:20 +0800

    修改版本

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
 import App from './APP.vue'
-import { Header } from 'mint-ui';
+import { Header,Swipe, SwipeItem } from 'mint-ui';
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+import VueRouter from 'vue-router'
+import router from './router.js'
+import VueResource from 'vue-resource'
+
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.use(router)
+
+
 
 Vue.component(Header.name, Header);
 
 var vm =new Vue({
     el:"#app",
-    render: c =>c(App)
+    render: c =>c(App),
+    router
 }) 
  console.log('hello')
--- src/router.js
+++ src/router.js
@@ -1,10 +1,24 @@
 import VueRouter from 'vue-router'//导入路由组件
+import homepage from './components/tabbar/homepage.vue'
+import vip from './components/tabbar/vip.vue'
+import shopping from './components/tabbar/shopping.vue'
+import search from './components/tabbar/search.vue'
 
 
 var router=new VueRouter({
-    routes:[    
+    routes:[
+        {path: '/',
+        redirect: '/homepage',//设置默认指向的路径
+        },{
+        path:'/homepage',component:homepage
+    },
+    {path:'/vip',component:vip},
+    {path:'/shopping',component:shopping},
+    {path:'/search',component:search}
 
-    ]
+
+    ],
+    linkActiveClass:'mui-active' //为路由激活时动态添加的类，类名可以自定义
     })
 
     export default router
